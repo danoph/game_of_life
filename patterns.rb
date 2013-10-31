b04a6279364531557ad9e3e50ea293f257841207
Daniel Errante <danoph@gmail.com>
2013-10-31 15:29:01 -0500

    loading random seeds from the internet

--- patterns.rb
+++ patterns.rb
@@ -1,3 +1,6 @@
+require 'open-uri'
+require 'nokogiri'
+
 require_relative 'game_of_life'
 require_relative 'pattern_loader'
 
@@ -38,8 +41,22 @@ threeships = "http://conwaylife.com/patterns/3enginecordership.cells"
 boats = "http://conwaylife.com/patterns/4boats.cells"
 fumaroles = "http://conwaylife.com/patterns/2fumaroles.cells"
 bricklayer = "http://conwaylife.com/patterns/bricklayer.cells"
+bunnies = "http://conwaylife.com/patterns/bunnies11.cells"
 
-cells_from_internet = PatternLoader.new(bricklayer, world).cells
+def hrefs
+  patterns_page = "http://conwaylife.com/patterns"
+  doc = Nokogiri::HTML(open(patterns_page))
+  links = doc.css('a')
+  hrefs = links.map {|link| link.attribute('href').to_s}.uniq.sort.delete_if {|href| href.empty?}.select{|href| href.match /\.cell/ }
+end
+
+def build_url(href)
+  "http://conwaylife.com/patterns/#{href}"
+end
+
+random_link = build_url(hrefs.sample)
+
+cells_from_internet = PatternLoader.new(random_link, world).cells
 
 game = GameOfLife.new(world, cells_from_internet, 39, 178)
 
